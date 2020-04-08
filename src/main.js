12a13c940e6472dbe86821b8f1e26504aedd60fa
khairul-abdi <kabdi384@gmail.com>
2020-04-08 15:12:01 +0700

    initial commit & add Bootstrap 4 to Vue-CLI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import jQuery from 'jquery'
+
+window.$ = window.jQuery = jQuery
+
+import 'popper.js'
+import 'bootstrap'
+import './assets/app.scss'
+
+
 
 Vue.config.productionTip = false
 
