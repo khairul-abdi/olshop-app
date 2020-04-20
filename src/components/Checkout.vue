778266c2ed74657148de587908e41e0b50af98f6
khairul-abdi <kabdi384@gmail.com>
2020-04-21 01:42:25 +0700

    Firebase init done

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -34,9 +34,9 @@
     </div>
     <Login></Login>
 
-    <div class="container mt-5 pt-5">
+    <div class="container table-responsive main pt-5">
       <div class="row">
-        <div class="col-md-9">
+        <!-- <div class="col-md-9">
           <h4 class="py-4">Checkout page</h4>
           <ul>
             <li v-for="(item, index) in this.$store.state.cart" :key="index" class="media">
@@ -50,9 +50,60 @@
               </div>
             </li>
           </ul>
-        </div>
+        </div> -->
         <div class="col-md-9">
-          Total Price
+          <table class="table table-responsive table-stipped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Items</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in this.$store.state.cart" :key="index" >
+                <td>
+                  <!-- <span @click="$store.commit('removeFromCart', item)" class="remove-item"></span> -->
+                </td>
+                <td>
+                  <img :src="item.productImage" style="width:80px">
+                  {{ item.productName }}
+                </td>
+                <td class="align-middle">
+                  <div class="center">
+                    <div class="input-group">
+                      <span class="input-group-btn">
+                        <button 
+                          type="button" 
+                          class="btn btn-danger mr-1" 
+                          @click="decreaseQty(item.productId)"
+                        >
+                          <i class="fa fa-minus fa-xs"></i>
+                        </button>
+                      </span>
+                      <input type="text" :value="item.productQuantity" class="form-control input-number rounded" style="width:40px">
+                      <span class="input-group-btn">
+                      <button
+                        type="button"
+                        class="btn btn-success ml-1"
+                        @click="increaseQty(item.productId)"
+                      >
+                        <i class="fa fa-plus fa-xs"></i>
+                      </button>
+                    </span>
+                    </div>
+                  </div>
+                </td>
+                <td class="align-middle">{{ item.productPrice | currency }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-md-3">
+          Total Price :{{ this.$store.getters.totalPrice | currency }}
+          
+          <button class="btn btn-primary mt-4"><i class="fas fa-cart-arrow-down mr-2"></i>Proceed Checkout</button>
         </div>
 
       </div>
@@ -68,45 +119,58 @@ export default {
   name: 'Checkout',
   components: {
     Login
+  },
+  methods: {
+    increaseQty(id) {
+      this.$store.commit('increment', id)
+    },
+    decreaseQty(id) {
+      this.$store.commit('decrement', id)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 
-  .nav-item a.nav-link{
-    font-size: 18px;
-    font-weight: bold;
-  }
+.nav-item a.nav-link{
+  font-size: 18px;
+  font-weight: bold;
+}
 
-  a.active{
-    color: #2c3e50 !important;
-  }
+a.active{
+  color: #2c3e50 !important;
+}
 
-  .nav-item a.nav-link:hover{
-    color: #42b983 !important;
-    cursor: pointer;
-    transition: all 0.5s;
-  }
+.nav-item a.nav-link:hover{
+  color: #42b983 !important;
+  cursor: pointer;
+  transition: all 0.5s;
+}
 
-  @media (min-width: 992px) { 
-    .navbar.custom-nav {
-      background-color: #fff !important;
-    }
+@media (min-width: 992px) { 
+  .navbar.custom-nav {
+    background-color: #fff !important;
   }
+}
 
-  .float-right{
-    color: #7f7f7f;
-    font-size: 16px;
-    text-shadow: 0 1px 0 #fff;
-    font-weight: bold;
-    line-height: 1;
-  }
+  .remove-item {
+  color: #7f7f7f;
+  font-size: 16px;
+  text-shadow: 0 1px 0 #fff;
+  font-weight: bold;
+  line-height: 1;
+}
 
-  .float-right:hover {
-    cursor: pointer;
-    color: #3f3f3f;
-    transition: all 0.5s;
-  }
+.remove-item:hover {
+  cursor: pointer;
+  color: #3f3f3f;
+  transition: all 0.5s;
+}
+
+.checkout div.main {
+  margin-top: 100px !important;
+}
+
 
 </style>
--- src/store.js
+++ src/store.js
@@ -8,6 +8,16 @@ export default new Vuex.Store({
   state: {
     cart: cart ? JSON.parse(cart) : []
   },
+  getters: {
+    totalPrice: state => {
+      let total = 0
+      state.cart.filter(item => {
+        total += (item.productPrice * item.productQuantity)
+      })
+
+      return total
+    }
+  },
   mutations: {
     addToCart(state, item) {
       let found = state.cart.find(product => product.productId == item.productId)
@@ -26,6 +36,31 @@ export default new Vuex.Store({
     removeFromCart(state, item) {
       let index = state.cart.indexOf(item)
       state.cart.splice(index, 1)
+
+      this.commit('saveData')
+    },
+    increment(state, id) {
+      let found = state.cart.find(product => product.productId == id)
+
+      if(found) {
+        found.productQuantity++
+      } else {
+        state.cart.push(item)
+      }
+
+      this.commit('saveData')
+    },
+    decrement(state, id) {
+      let found = state.cart.find(product => product.productId == id)
+
+      if(found.productQuantity > 1) {
+        found.productQuantity--
+      }
+
+      if(found.productQuantity == 1) {
+        // this.removeFromCart()
+        this.commit('removeFromCart',state,this.state.cart)
+      }
 
       this.commit('saveData')
     }
