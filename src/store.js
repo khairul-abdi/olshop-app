50d5c65a1a4e5deabb2641c0a177d17512c5f741
khairul-abdi <kabdi384@gmail.com>
2020-04-20 15:03:14 +0700

    Cart Persistent in Vuex

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
+
+let cart = window.localStorage.getItem('cart')
 
 export default new Vuex.Store({
   state: {
-    cart: []
+    cart: cart ? JSON.parse(cart) : []
   },
   mutations: {
     addToCart(state, item) {
@@ -16,6 +17,11 @@ export default new Vuex.Store({
       } else {
         state.cart.push(item)
       }
+
+      this.commit('saveData')
+    },
+    saveData(state) {
+      window.localStorage.setItem('cart', JSON.stringify(state.cart))
     }
   }
 })
